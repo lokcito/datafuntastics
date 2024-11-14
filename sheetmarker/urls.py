274aba0a56aec541660dd70914e105b5f3d8e165
ray <enciso.leo@gmail.com>
2024-11-14 15:07:39 -0800

    implementados 3 urls

--- sheetmarker/urls.py
+++ sheetmarker/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+
+#ruta_para_macros = 
+# URL Pattern =>  Parte de la URL | funcion     | Darle nombre a la url
+                #    /macros      | Referencia  | Para ubicar a la url con codigo
+
 urlpatterns = [
-    path("", views.v_index, name="v_index"),
+    path("", views.v_index, name="v_index"), #/
+    path("macros", views.v_macro, name="macros" ),
+    path("powerbi", views.v_powerbi, name="powerbi"),
+    path("analitica", views.v_analitica, name="analitica"),
     path("reporte_xls", views.v_reporte_xls, name="v_reporte_xls")
 ]
--- sheetmarker/views.py
+++ sheetmarker/views.py
@@ -7,6 +7,15 @@ from openpyxl import Workbook
 def v_index(request):
     return HttpResponse("Sheetmarker index")
 
+def v_macro(request):
+    return render(request, "sheetmarker/macros.html")
+
+def v_powerbi(request):
+    return render(request, "sheetmarker/powerbi.html")
+
+def v_analitica(request):
+    return render(request, "sheetmarker/analitica.html")
+
 def v_reporte_xls(request):
     # Crear un libro de Excel
     workbook = Workbook()
